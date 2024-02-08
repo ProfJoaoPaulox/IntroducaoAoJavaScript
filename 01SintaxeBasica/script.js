1c2bf52a08e22f692f221e705baf54a077d409fc
Professor João Paulo <125033758+ProfJoaoPaulox@users.noreply.github.com>
2024-02-08 11:43:56 -0300

    Sitaxe básica

---
+++ 01SintaxeBasica/script.js
@@ -0,0 +1,43 @@
+// COMENTÁRIOS
+
+
+// A sintaxe dos comentários em JavaScript é semelhante como em C++ e em muitas outras linguagens:
+// Os comentários serão ignorados pela máquina quando o app estiver em execução, geralmente são 
+// utilizados para anotações em meio ao código, muito prático quando estamos trabalhando em equipe 
+
+// EXEMPLO 1
+
+
+/*
+    EXEMPLO 2
+*/
+
+// DECLARAÇOES DE VARIÁVEIS
+
+var Variavel_1 = 10;
+// Declara uma variável, opcionalmente, inicializando-a com um valor.
+
+let Variavel_2 = 20;
+// Declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor.
+
+const Variavel_3 = 30; 
+//Declara uma constante de escopo de bloco, apenas de leitura.
+
+
+// console.log(): Esta função é utilizada para exibir mensagens de depuração ou informações no 
+// console do navegador. É muito útil durante o desenvolvimento para verificar valores de variáveis, 
+// depurar código e entender como o programa está se comportando. Por exemplo:
+
+console.log("Olá mundo!");
+
+
+// alert(): Esta função exibe uma caixa de diálogo de alerta no navegador com a mensagem especificada.
+// É frequentemente utilizada para fornecer feedback imediato ao usuário. Por exemplo:
+
+alert("Clique em OK para continuar.");
+
+
+
+
+
+
